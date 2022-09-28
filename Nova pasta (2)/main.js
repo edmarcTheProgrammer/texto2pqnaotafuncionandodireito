4dd522d150ffab070924d29c45c9fce3db1a426d
edmarcTheProgrammer <108365053+edmarcTheProgrammer@users.noreply.github.com>
2022-09-28 18:31:16 -0400

    Add files via upload

---
+++ Nova pasta (2)/main.js
@@ -0,0 +1,16 @@
+var texto = "bom dia!!!!!!!!!"
+console.log(texto)
+
+var nome = "ALFREDO ";
+var nomedoisavolta = "ALDREFEFAL";
+var nometresalvineosesquilos = nome.concat(nomedoisavolta);
+console.log(nometresalvineosesquilos);
+
+var a = 10;
+console.log(a);
+var b = 5;
+console.log(b);
+var add = a + b;
+console.log(add);
+var sub = a - b;
+console.log(sub);
